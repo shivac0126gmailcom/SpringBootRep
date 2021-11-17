e09f2c86b95a426008552520bf1e74ef5d182465
Kiran <Kiran@192.168.0.8>
2021-11-18 00:06:58 +0530

    Ticket develop branch 2nd commit

--- src/main/java/com/example/demo/MessageRunner.java
+++ src/main/java/com/example/demo/MessageRunner.java
@@ -12,6 +12,8 @@ public class MessageRunner implements CommandLineRunner{
 		System.out.println("this spring boot Application....");
 		System.out.println("this modified file..");
 		System.out.println("this is developer branch do you know this..");
+		
+		System.out.println("this is developer branch do you know this.. 2nd one");
 	}
 
 }
